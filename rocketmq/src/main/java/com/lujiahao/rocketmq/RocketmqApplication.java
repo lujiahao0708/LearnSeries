5b55dfe5870bf1ee94786853d94c867b5cfbafbe
lujiahao0708 <lujiahao0708@gmail.com>
2018-05-26 12:37:23 +0800

    init project

---
+++ rocketmq/src/main/java/com/lujiahao/rocketmq/RocketmqApplication.java
@@ -0,0 +1,12 @@
+package com.lujiahao.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqApplication.class, args);
+    }
+}
