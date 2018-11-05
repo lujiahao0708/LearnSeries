30b3f22c960acda403f9752cf615589df4fb42cb
lujiahao0708 <lujiahao0708@gmail.com>
2018-11-05 16:05:59 +0800

    update

--- LearnDocker/rocketmq/4.3.0/play-docker.sh
+++ LearnDocker/rocketmq/4.3.0/play-docker.sh
@@ -5,10 +5,10 @@ docker build -t apache/rocketmq-base:4.3.0 --build-arg version=4.3.0 ./rocketmq-
 
 # Build namesrv and broker
 docker build -t apache/rocketmq-namesrv:4.3.0 ./rocketmq-namesrv
-docker build -t apache/rocketmq-broker:4.3.0 ./rocketmq-broker
+docker build -t apache/rocketmq-broker:4.3.0 ./rocketmq-broker --build-arg host_ip=111.231.199.76
 
 # Run namesrv and broker
 
 docker run -d -p 9876:9876 --name rmqnamesrv  apache/rocketmq-namesrv:4.3.0
-docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" -e "HOST_IP=111.231.199.76" apache/rocketmq-broker:4.3.0
+docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" apache/rocketmq-broker:4.3.0
 
