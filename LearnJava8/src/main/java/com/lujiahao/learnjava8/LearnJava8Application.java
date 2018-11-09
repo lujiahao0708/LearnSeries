3750499301af6390ff0c87ca6697973ab4288995
lujiahao0708 <lujiahao0708@gmail.com>
2018-11-09 09:37:17 +0800

    LearnJava8

---
+++ LearnJava8/src/main/java/com/lujiahao/learnjava8/LearnJava8Application.java
@@ -0,0 +1,12 @@
+package com.lujiahao.learnjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJava8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJava8Application.class, args);
+    }
+}
