f8d5ec3d8676e56234cef06d175e13246086f58c
lujiahao <lujiahao@01zhuanche.com>
2019-07-22 10:16:55 +0800

    add shiro

---
+++ LearnShiro/shiro-first/src/main/java/com/lujiahao/shirofirst/ShiroFirstApplication.java
@@ -0,0 +1,13 @@
+package com.lujiahao.shirofirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroFirstApplication.class, args);
+    }
+
+}
---
+++ LearnShiro/shiro-mybatis-h2/src/main/java/com/lujiahao/shiromybatish2/ShiroMybatisH2Application.java
@@ -0,0 +1,13 @@
+package com.lujiahao.shiromybatish2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroMybatisH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroMybatisH2Application.class, args);
+    }
+
+}
