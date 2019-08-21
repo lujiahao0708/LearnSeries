943ee6a358ec84a791129c7feaea635fb78b8e7f
lujiahao <lujiahao@01zhuanche.com>
2019-08-21 23:05:58 +0800

    03.2 Eureka-自我保护和服务发现

--- LearnSpringCloud/atguigu-microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/hellodev/controller/DeptController_Consumer.java
+++ LearnSpringCloud/atguigu-microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/hellodev/controller/DeptController_Consumer.java
@@ -35,4 +35,12 @@ public class DeptController_Consumer {
     public List<Dept> list() {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
     }
+
+
+    //测试@EnableDiscoveryClient,消费端可以调用服务发现
+    @RequestMapping(value="/consumer/dept/discovery")
+    public Object discovery() {
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/discovery", Object.class);
+    }
+
 }
--- LearnSpringCloud/atguigu-microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/hellodev/controller/DeptController.java
+++ LearnSpringCloud/atguigu-microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/hellodev/controller/DeptController.java
@@ -2,7 +2,10 @@ package com.hellodev.controller;
 
 import com.hellodev.entities.Dept;
 import com.hellodev.service.DeptService;
+//import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,4 +34,18 @@ public class DeptController {
         return service.list();
     }
 
+    @Autowired
+    private DiscoveryClient client;
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
 }
