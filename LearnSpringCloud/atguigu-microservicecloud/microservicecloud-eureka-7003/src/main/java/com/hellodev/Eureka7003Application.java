7ffb9fcb1c6e8bb0e40928c2b6def17d6a219440
lujiahao <lujiahao@01zhuanche.com>
2019-08-22 15:25:47 +0800

    03.3 Eureka-集群实战

---
+++ LearnSpringCloud/atguigu-microservicecloud/microservicecloud-eureka-7003/src/main/java/com/hellodev/Eureka7003Application.java
@@ -0,0 +1,16 @@
+package com.hellodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// EurekaServer服务器端启动类,接受其它微服务注册进来
+@EnableEurekaServer
+public class Eureka7003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eureka7003Application.class, args);
+	}
+
+}
