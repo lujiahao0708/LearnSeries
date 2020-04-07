c48e3be2adfc1933e22d7a1656e70c4a935ce1f5
lujiahao0708 <lujiahao0708@gmail.com>
2020-04-07 16:43:21 +0800

    redis-single-jedis

---
+++ LearnRedis/code/redis-single-jedis/src/main/java/club/hellodev/redis/single/jedis/RedisSingleJedisApplication.java
@@ -0,0 +1,13 @@
+package club.hellodev.redis.single.jedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSingleJedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSingleJedisApplication.class, args);
+	}
+
+}
