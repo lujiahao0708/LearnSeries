6409460f95b02f8d93b15ec95a03c475a5b43406
lujiahao0708 <lf_studio_sjz@126.com>
2020-06-02 23:04:13 +0800

    批量模式

---
+++ LearnRocketMQ/code/rocketmq-base/src/main/java/com/lujiahao/msg/producer/BatchProducer.java
@@ -0,0 +1,47 @@
+package com.lujiahao.msg.producer;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 发送批量消息
+ * @author lujiahao
+ * @date 2020-05-29
+ */
+public class BatchProducer {
+
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者 Producer
+        DefaultMQProducer producer = new DefaultMQProducer("sync_msg_group");
+        // 设置 nameserver地址
+        producer.setNamesrvAddr("localhost:9876");
+        // 启动 producer 实例
+        producer.start();
+
+        List<Message> messageList = IntStream.range(0, 10).mapToObj(r -> {
+            // 创建消息,并制定 topic/tag和消息体
+            Message message = null;
+            try {
+                message = new Message("TopicTest", "TagA", ("Hello RocketMQ " + r).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return message;
+        }).collect(Collectors.toList());
+
+        // 发送消息到broker
+        SendResult sendResult = producer.send(messageList);
+        // 输出发送结果
+        System.out.printf("%s%n", sendResult);
+
+        // 关闭 producer 实例
+        producer.shutdown();
+    }
+}
