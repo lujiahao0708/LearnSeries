36bb4d4f0d41e2065cd7e95dc6e9a8ff27f01c38
lujiahao0708 <lf_studio_sjz@126.com>
2020-08-25 16:17:51 +0800

    update

--- LearnRocketMQ/code/data-producer/src/main/java/com/lujiahao/service/SendMsgService.java
+++ LearnRocketMQ/code/data-producer/src/main/java/com/lujiahao/service/SendMsgService.java
@@ -6,8 +6,42 @@ import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class SendMsgService {
 
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer_9876 = new DefaultMQProducer("TestGroup_9876");
+        producer_9876.setNamesrvAddr("192.168.12.8:9876");
+        producer_9876.setInstanceName("instanceName_9876");
+        producer_9876.start();
+        System.out.println("Producer_9876属性信息:" + producer_9876);
+
+        Message message_9876 = new Message(
+                "TestTopic1", "TagA",
+                "9876 " + LocalDateTime.now(),
+                "Hello RocketMQ!".getBytes(RemotingHelper.DEFAULT_CHARSET));
+        SendResult sendResult_9876 = producer_9876.send(message_9876);
+        System.out.println("Producer_9876发送结果:" + sendResult_9876);
+
+
+        DefaultMQProducer producer_9877 = new DefaultMQProducer("TestGroup_9877");
+        producer_9877.setNamesrvAddr("192.168.12.8:9877");
+        producer_9877.setInstanceName("instanceName_9877");
+        producer_9877.start();
+        System.out.println("Producer_9877属性信息:" + producer_9877);
+
+        Message message_9877 = new Message(
+                "TestTopic2", "TagA",
+                "9877 " + LocalDateTime.now(),
+                "Hello RocketMQ!".getBytes(RemotingHelper.DEFAULT_CHARSET));
+        SendResult sendResult_9877 = producer_9877.send(message_9877);
+        System.out.println("Producer_9877发送结果:" + sendResult_9877);
+
+        // producer 最后再关闭
+        producer_9876.shutdown();
+        producer_9877.shutdown();
+    }
 
 }
